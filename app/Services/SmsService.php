d44f23e8071d2b3c54cf4cd9af3308295e1c9392
Aayush Patidar <aayushpatidar04@gmail.com>
2026-08-22 22:21:24 +0530

    sms otp changes 2.0

--- app/Services/SmsService.php
+++ app/Services/SmsService.php
@@ -33,7 +33,7 @@ class SmsService
 
         $phone = $this->normalizePhone($phone);
 
-        $message = "Your OTP for registering for ARIHANT PLUS AI & ALGO CONCLAVE, scheduled on 5th September 2026 at Marriott Hotel, Indore, is {$otp}.\n\nThis OTP is valid for 5 minutes. By entering this OTP, you provide your consent to register for the event.\n\nArihant Capital Markets Limited";
+        $message = "Your OTP for registering for ARIHANT PLUS AI & ALGO CONCLAVE, scheduled on 5th September 2026 at Marriott Hotel, Indore, is {$otp}.\nThis OTP is valid for 5 minutes. By entering this OTP, you provide your consent to register for the event.\nArihant Capital Markets Limited";
 
         return $this->dispatch($phone, $message);
     }
