deb1140f6a352837963f9bb0d7c93261ee0115c7
Aayush Patidar <aayushpatidar04@gmail.com>
2026-08-26 11:03:40 +0530

    type with lead api

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -178,6 +178,7 @@ class RegistrationController extends Controller
                 'city' => $reg->city,
                 'sourceUrl' => 'https://event.arihantplus.com', // or your event landing page
                 'source' => 'AI & Algo Conclave',
+                'type' => $reg->is_subbroker ? 'Sub-broker' : ($reg->is_existing_client ? 'Existing Client' : 'New Client'),
             ]);
 
         } catch (\Exception $e) {
@@ -342,6 +343,7 @@ class RegistrationController extends Controller
                 'city' => $reg->city,
                 'sourceUrl' => 'https://event.arihantplus.com',
                 'source' => 'AI & Algo Conclave',
+                'type' => $reg->is_subbroker ? 'Sub-broker' : ($reg->is_existing_client ? 'Existing Client' : 'New Client'),
             ]);
 
         } catch (\Exception $e) {
